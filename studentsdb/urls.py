6a66e7c6b17c4a436fd0f719837b8af2cb58b795
Dmytro Rybalko <mail4dmytro@gmail.com>
2015-11-23 18:16:42 +0200

    hello word is on localhost

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -17,5 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    # Students urls
+    url(r'^$', 'students.views.students_list', name='home'),
+
     url(r'^admin/', include(admin.site.urls)),
 ]
