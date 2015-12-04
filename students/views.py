2f57572e12b02829bee55fa2006350268f6f91ea
Dmytro Rybalko <mail4dmytro@gmail.com>
2015-12-04 21:32:47 +0200

    replace table add cycle for

--- students/views.py
+++ students/views.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id':1,
+         'first_name': u'Дмитро',
+         'last_name': u'Рибалко',
+         'ticket': 235,
+         'image': 'img/my_bike.jpg'},
+        {'id': 2,
+            'first_name': u'Вікторія',
+            'last_name': u'Якуніна',
+            'ticket': 2123,
+            'image': 'img/vika.jpg'},
+        {'id': 3,
+            'first_name': u'Корост',
+            'last_name': u'Андрій',
+            'ticket': 2125,
+            'image': 'img/GoITnewLife.jpg'}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Students Add Form</h1>')
