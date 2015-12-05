96a5a7b5d10b5ef5d044bb8475e6587dd1896493
Dmytro Rybalko <mail4dmytro@gmail.com>
2015-12-05 18:09:23 +0200

    embed base tamplete into students_list.html

--- students/views.py
+++ students/views.py
@@ -37,7 +37,7 @@ def students_delete(request, sid):
 # Vies for Groups
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    return render(request, 'students/group_list.html', {})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
