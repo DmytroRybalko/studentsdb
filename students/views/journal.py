85e3e7531c1b6c6351bc57385eaffcdc68d360af
Dmytro Rybalko <mail4dmytro@gmail.com>
2015-12-20 14:56:18 +0200

    add list of students as django template in student_log.html

--- students/views/journal.py
+++ students/views/journal.py
@@ -3,9 +3,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from calendar_generator import calendar_of_month
-
+from students import students_list
 
 def students_log(request):
     calendar = calendar_of_month(2015,11)
     #calendar = ({'date': 1, 'day': u'Пн'}, {'date': 2, 'day':u'Вт'}, ...)
-    return render(request, 'students/student_log.html', {'calendar':calendar})
+    students = (
+        {'id': 1,
+         'name': u'Дмитро\nРибалко'},
+        {'id': 2,
+         'name': u'Вікторія\nЯкуніна'},
+        {'id': 3,
+         'name': u'Віктор\nКоцюба'}
+    )
+    return render(request, 'students/student_log.html', {'calendar':calendar,
+                                                         'students':students})
