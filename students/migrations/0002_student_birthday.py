8b71214b41b08ff229cdd1be5d6cdc2f4d30ce21
Dmytro Rybalko <mail4dmytro@gmail.com>
2016-01-04 15:52:01 +0200

    initialize database and setup admin interface

---
+++ students/migrations/0002_student_birthday.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('students', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='student',
+            name='birthday',
+            field=models.DateField(null=True, verbose_name='\u0414\u0430\u0442\u0430 \u043d\u0430\u0440\u043e\u0434\u0436\u0435\u043d\u043d\u044f'),
+        ),
+    ]
