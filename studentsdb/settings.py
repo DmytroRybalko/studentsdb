02fa364d4b3c057ff21f17d865882389f6b33934
Dmytro Rybalko <mail4dmytro@gmail.com>
2016-01-18 11:48:42 +0200

    migrate to mysql and move DATABASE from settings.py to db.py

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.conf import global_settings
+from virtualenvs.studentsdb.src.db import DATABASES
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,12 +78,16 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    }
-}
+DATABASES = DATABASES
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': 'localhost',
+#         'USER': 'students_db_user',
+#         'PASSWORD': 'user_db',
+#         'NAME': 'students_db',
+#     }
+# }
 
 
 # Internationalization
