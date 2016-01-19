10bed8485d4bbe47234def1f72fb88ce5acd5f9b
Dmytro Rybalko <mail4dmytro@gmail.com>
2016-01-19 13:35:01 +0200

    create form for adding students without validation

--- students/views/students.py
+++ students/views/students.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from ..models import Student
+from ..models import Student, Group
 # Views for Students
 
 def students_list(request):
@@ -33,7 +34,50 @@ def students_list(request):
     return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
-    return HttpResponse('<h1>Students Add Form</h1>')
+    # was form posted?
+    if request.method == "POST":
+        # was form add button clicked?
+        if request.POST.get('add_button') is not None:
+
+            # TODO: validate input from users
+            errors = {}
+
+            if not errors:
+                # create student object
+                student = Student(
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    middle_name=request.POST['middle_name'],
+                    birthday=request.POST['birthday'],
+                    ticket=request.POST['ticket'],
+                    student_group=Group.objects.get(
+                        pk=request.POST['student_group']),
+                    photo=request.FILES['photo'],
+                )
+
+                # save it to database
+                student.save()
+
+                # redirect user to students list
+                return HttpResponseRedirect(reverse('home'))
+
+            else:
+                # render from with errors and previous user input
+                return render(request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title'),
+                     'errors': errors})
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to home page on cancel button
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        # initial form render
+        return render(request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title')})
+
+    # Якщо форма не була запощена:
+        # Повертаємо код початкового стану форми
+    return render(request, 'students/students_add.html',
+        {'groups': Group.objects.all().order_by('title')})
 
 def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' % sid)
