6eb100b47d9dfb9cb481d21c8a84713711ebb4b4
Dmytro Rybalko <mail4dmytro@gmail.com>
2016-03-20 19:22:35 +0200

    add sorting, paginator for groups list

--- students/views/groups.py
+++ students/views/groups.py
@@ -2,22 +2,45 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-
-groups = (
-        {'id':1,
-            'group_name': u'ВЛ-81',
-            'steward': u'Баличева Анна'},
-        {'id': 2,
-            'group_name': u'ВА-81',
-            'steward': u'Якуніна Вікторія'},
-        {'id': 3,
-            'group_name': u'ВТ-81',
-            'steward': u'Корост Андрій'}
-    )
+from ..models import Student, Group
 
 def groups_list(request):
-    return render(request, 'students/group_list.html', {'groups': groups})
+    # Set field name for ordering by default
+    order_by_default = 'title'
+    groups = Group.objects.all()
+
+    # Collect sorted group's id into separate list
+    groups_pk =[pk.pk for pk in groups]
+
+    # Get list of all model's fields name
+    fields = Group._meta.get_all_field_names()
+
+    # try to order students list
+    order_by = request.GET.get('order_by', order_by_default)
+    if order_by in fields:
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    # paginator groups
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an Integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        groups = paginator.page(paginator.num_pages)
+
+    return render(request, 'students/group_list.html',
+                  {'groups': groups,
+                   'groups_pk': groups_pk,
+                   'order_by_default':order_by_default})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
