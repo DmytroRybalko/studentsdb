50300f981477f3d62fc2dbf5d06211bc0ecaee15
Dmytro Rybalko <mail4dmytro@gmail.com>
2016-04-22 16:45:54 +0300

    move paginator from group_list.py to util.py -> django_paginator and includes/django_paginator.html

--- students/util.py
+++ students/util.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def make_order_by(model_fields, query_set, order_by, reverse):
 
@@ -6,4 +7,17 @@ def make_order_by(model_fields, query_set, order_by, reverse):
         query_set = query_set.order_by(order_by)
         if reverse == '1':
             query_set = query_set.reverse()
+    return query_set
+
+def django_paginator(query_set, page, row_per_page):
+    paginator = Paginator(query_set, row_per_page)
+    try:
+        query_set = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an Integer, deliver first page.
+        query_set = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        query_set = paginator.page(paginator.num_pages)
     return query_set
--- students/views/groups.py
+++ students/views/groups.py
@@ -7,35 +7,29 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models.students import Student
 from ..models.groups import Group
 
-from ..util import make_order_by
+from ..util import make_order_by, django_paginator
 
 def groups_list(request):
     # Set field name for ordering by default
     order_by_default = 'title'
-    groups = Group.objects.all()
+    query_set = Group.objects.all()
      # Get list of all model's fields name
     fields = Group._meta.get_all_field_names()
     order_by = request.GET.get('order_by', order_by_default)
     reverse = request.GET.get('reverse', '')
-    # try to order groups list
-    groups = make_order_by(fields, groups, order_by, reverse)
+    # try to order group list
+    query_set = make_order_by(fields, query_set, order_by, reverse)
 
     # paginator groups
-    paginator = Paginator(groups, 3)
+    row_per_page = 3
     page = request.GET.get('page')
-    try:
-        groups = paginator.page(page)
-    except PageNotAnInteger:
-        # if page is not an Integer, deliver first page.
-        groups = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver
-        # last page of results.
-        groups = paginator.page(paginator.num_pages)
+    query_set = django_paginator(query_set, page, row_per_page)
 
     return render(request, 'students/group_list.html',
-                  {'groups': groups,
-                   'order_by_default':order_by_default})
+                  {'order_by_default': order_by_default,
+                   'query_set': query_set,
+                   'order_by': order_by,
+                   'reverse': reverse})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
